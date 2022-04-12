4b4e01bffdfe348c69a71df4297435f12ae32928
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-04-13 02:36:29 +0530

    Delete src/components directory

--- src/components/heading.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Heading = () => {
-    return (
-        <h1 className="heading">Styling Using Functional and class components</h1>
-    )
-}
-export default Heading;
